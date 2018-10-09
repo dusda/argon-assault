0c5aeaf8cd66b7a263f9eb1f0d1ac8eddb139f87
dusda <dustin.dahl@gmail.com>
2018-10-09 10:29:21 -0600

    Added a splash screen and some music.
    I'm in the middle of S4L80. Not done yet.

---
+++ src/Argon Assault/Assets/Splash.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    AudioSource _music;
+
+    void Start()
+    {
+        _music = GetComponent<AudioSource>();
+        Invoke("LoadLevel", 1f);
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
