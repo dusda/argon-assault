00efd38fc8a08790f6f6b064885334626ba5d01e
dusda <dustin.dahl@gmail.com>
2018-10-09 20:47:24 -0600

    Clamped ship movement. S4L86

--- src/Argon Assault/Assets/Player.cs
+++ src/Argon Assault/Assets/Player.cs
@@ -3,9 +3,22 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField, Tooltip("In m/sec")]
+    float xSpeed = 4f;
+
+    [SerializeField, Tooltip("In m")]
+    float xRange = 2.58f;
+
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xSpeed * xThrow * Time.deltaTime;
+        float rawX = transform.localPosition.x + xOffset;
+        float x = Mathf.Clamp(rawX, -xRange, xRange);
+
+        transform.localPosition = new Vector3(
+            x,
+            transform.localPosition.y,
+            transform.localPosition.z);
     }
 }
