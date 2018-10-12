2bdf23ec6cf77125715e144c10f1460963928b73
dusda <dustin.dahl@gmail.com>
2018-10-11 21:57:31 -0600

    Enemies explode.
    They blow up, and the effects clean themselves up. They parent themselves to a trash can gameobject, then destroy themselves after 5 seconds (well after the effect itself is done playing). S4L102

---
+++ src/Argon Assault/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFx;
+    [SerializeField] Transform parent;
+
+    void Start()
+    {
+        Collider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFx, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ src/Argon Assault/Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+    void Start()
+    {
+        //todo allow customization
+        Destroy(gameObject, 5f);
+    }
+}
