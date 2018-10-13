09dc21b2ada4a468752d60a6e2bd85231a2710ad
dusda <dustin.dahl@gmail.com>
2018-10-13 17:42:16 -0600

    Added a scoreboard., S4L104

---
+++ src/Argon Assault/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int _score;
+    Text _scoreText;
+
+    [SerializeField] int scorePerHit = 12;
+
+    void Start()
+    {
+        _scoreText = GetComponent<Text>();
+        _scoreText.text = _score.ToString();
+    }
+
+    public void ScoreHit()
+    {
+        _score += scorePerHit;
+        _scoreText.text = _score.ToString();
+    }
+}
